e454bf806d69c1f60e5c7acbd14285a04ead7741
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-06-14 11:51:58 -0400

    Final

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,21 @@
 class SignupsController < ApplicationController
+    def create
+        signup = Signup.create!(signup_params)
+        if signup
+            render json: signup.activity
+        else
+            bad_request
+        end
+
+    end
+
+    private
+
+    def signup_params
+        params.permit(:time, :camper_id, :activity_id)
+    end
+
+    def bad_request
+        render json: { error: "validation errors" }, status: :bad_request
+    end
 end
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -3,4 +3,7 @@ class Signup < ApplicationRecord
     belongs_to :activity
 
     validates :time, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
+    validates :camper_id, presence: true
+    validates :activity_id, presence: true
+
 end
